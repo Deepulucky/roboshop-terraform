8e0a5e707afd2699c681d05b6358d29ebd70cd18
Deepulucky <deepikathadi07@gmail.com>
2023-12-24 20:47:02 +0530

    kms

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -8,39 +8,117 @@ tags = {
   project_name  = "roboshop"
 }
 
-components   = {
-  frontend  = {
-    tags = { Monitor = "true" , env = "dev" }
-  }
-  mongodb   = {
-    tags = { env = "dev" }
-  }
-  catalogue = {
-    tags = { Monitor = "true" , env = "dev" }
-  }
-  redis     = {
-    tags = { env = "dev" }
-  }
-  user      = {
-    tags = { Monitor = "true" , env = "dev" }
-  }
-  cart      = {
-    tags = { Monitor = "true" , env = "dev" }
-  }
-  mysql     = {
-    tags = { env = "dev" }
-  }
-  shipping  = {
-    tags = { Monitor = "true" , env = "dev" }
-  } 
-  rabbitmq  = {
-    tags = { env = "dev" }
-  }
-  payment   = {
-    tags = { Monitor = "true" , env = "dev" }
-  }
 
+vpc = {
+  main = {
+    cidr_block = "10.0.0.0/16"
+    subnets = {
+      web    = { cidr_block = ["10.0.0.0/24", "10.0.1.0/24"] }
+      app    = { cidr_block = ["10.0.2.0/24", "10.0.3.0/24"] }
+      db     = { cidr_block = ["10.0.4.0/24", "10.0.5.0/24"] }
+      public = { cidr_block = ["10.0.6.0/24", "10.0.7.0/24"] }
+    }
+  }
 }
+
+default_vpc_id        = "vpc-053d5cff1ef7e1fd2"
+default_vpc_rt        = "rtb-0afb751f902c88904"
+allow_ssh_cidr        = ["172.31.0.0/16"]
+zone_id               = "Z019183117R4826VIH8DK" 
+kms_key_id            = "f0a71b80-90c3-4da1-a189-b4b95e9764e8"
+kms_key_arn           = "arn:aws:kms:us-east-1:739561048503:key/f0a71b80-90c3-4da1-a189-b4b95e9764e8"
+//allow_prometheus_cidr = ["172.31.95.219/32"]
+
+rabbitmq = {
+  main = {
+    instance_type = "t3.small"
+    component     = "rabbitmq"
+  }
+}
+
+rds = {
+  main = {
+    component      = "rds"
+    engine         = "aurora-mysql"
+    engine_version = "5.7.mysql_aurora.2.11.3"
+    db_name        = "dummy"
+    instance_count = 1
+    instance_class = "db.t3.small"
+  }
+}
+
+documentdb = {
+  main = {
+    component         = "docdb"
+    engine            = "docdb"
+    engine_version    = "4.0.0"
+    db_instance_count = 1
+    instance_class    = "db.t3.medium"
+  }
+}
+
+elasticache = {
+  main = {
+    component               = "elasticache"
+    engine                  = "redis"
+    engine_version          = "6.x"
+    replicas_per_node_group = 1
+    num_node_groups         = 1
+    node_type               = "cache.t3.micro"
+    parameter_group_name    = "default.redis6.x.cluster.on"
+  }
+}
+
+alb = {
+  public = {
+    name               = "public"
+    internal           = false
+    load_balancer_type = "application"
+    subnet_ref         = "public"
+  }
+  private = {
+    name               = "private"
+    internal           = true
+    load_balancer_type = "application"
+    subnet_ref         = "app"
+  }
+}
+
+# components   = {
+#   frontend  = {
+#     tags = { Monitor = "true" , env = "dev" }
+#   }
+#   mongodb   = {
+#     tags = { env = "dev" }
+#   }
+#   catalogue = {
+#     tags = { Monitor = "true" , env = "dev" }
+#   }
+#   redis     = {
+#     tags = { env = "dev" }
+#   }
+#   user      = {
+#     tags = { Monitor = "true" , env = "dev" }
+#   }
+#   cart      = {
+#     tags = { Monitor = "true" , env = "dev" }
+#   }
+#   mysql     = {
+#     tags = { env = "dev" }
+#   }
+#   shipping  = {
+#     tags = { Monitor = "true" , env = "dev" }
+#   } 
+#   rabbitmq  = {
+#     tags = { env = "dev" }
+#   }
+#   payment   = {
+#     tags = { Monitor = "true" , env = "dev" }
+#   }
+
+# }
+
+
 #component = 
 # vpc = {
 #   main = {
--- main.tf
+++ main.tf
@@ -1,45 +1,101 @@
-# module "vpc" {
-#   source = "git::https://github.com/Deepulucky/tf-module-vpc.git"
+module "vpc" {
+  source = "git::https://github.com/Deepulucky/tf-module-vpc.git"
 
-#   for_each   = var.vpc
-#   cidr_block = each.value["cidr_block"]
-#   subnets    = each.value["subnets"]
-#   tags       = local.tags
-#   env        = var.env
-# }
+  for_each   = var.vpc
+  cidr_block = each.value["cidr_block"]
+  subnets    = each.value["subnets"]
 
-# resource "aws_iam_policy" "policy" {
-#   name        = "${var.component}-${var.env}-ssm-pm-policy"
-#   path        = "/"
-#   description = "${var.component}-${var.env}-ssm-pm-policy"
+  env            = var.env
+  tags           = var.tags
+  default_vpc_id = var.default_vpc_id
+  default_vpc_rt = var.default_vpc_rt
 
-#   policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Sid" : "VisualEditor0",
-#         "Effect" : "Allow",
-#         "Action" : [
-#           "ssm:GetParameterHistory",
-#           "ssm:GetParametersByPath",
-#           "ssm:GetParameters",
-#           "ssm:GetParameter",
-#           "kms:Decrypt"
-#         ],
-#         "Resource" : concat([
-#           "arn:aws:ssm:us-east-1:739561048503:parameter/roboshop.${var.env}.${var.component}.*",
-#           "arn:aws:ssm:us-east-1:739561048503:parameter/roboshop.nexus.*",
-#           var.kms_arn
-#         ], var.extra_param_access)
-#       }
-#     ]
-#   })
-# }
-
-module "instances" {
-  for_each = var.components
-  source   = "git::https://github.com/Deepulucky/tf-module-app.git"
-  component = each.key
-  env       = var.env
-  tags      = merge(each.value["tags"], var.tags)
 }
+
+
+module "rabbitmq" {
+  source = "git::https://github.com/Deepulucky/tf-module-rabbitmq.git"
+
+  for_each      = var.rabbitmq
+  component     = each.value["component"]
+  instance_type = each.value["instance_type"]
+
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  subnet_id      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)[0]
+
+  env            = var.env
+  tags           = var.tags
+  allow_ssh_cidr = var.allow_ssh_cidr
+  zone_id        = var.zone_id
+  kms_key_arn    = var.kms_key_arn
+}
+
+module "rds" {
+  source = "git::https://github.com/Deepulucky/tf-module-rds.git"
+
+  for_each       = var.rds
+  component      = each.value["component"]
+  engine         = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  db_name        = each.value["db_name"]
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  instance_count = each.value["instance_count"]
+  instance_class = each.value["instance_class"]
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+
+  tags           = var.tags
+  env            = var.env
+  kms_key_arn    = var.kms_key_arn
+  sg_subnet_cidr = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+
+}
+
+module "documentdb" {
+  source = "git::https://github.com/Deepulucky/tf-module-documentdb.git"
+
+  for_each          = var.documentdb
+  component         = each.value["component"]
+  subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr    = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  engine            = each.value["engine"]
+  engine_version    = each.value["engine_version"]
+  db_instance_count = each.value["db_instance_count"]
+  instance_class    = each.value["instance_class"]
+
+  tags        = var.tags
+  env         = var.env
+  kms_key_arn = var.kms_key_arn
+}
+
+module "elasticache" {
+  source = "git::https://github.com/Deepulucky/tf-module-elasticache.git"
+
+  for_each                = var.elasticache
+  component               = each.value["component"]
+  subnet_ids              = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnet_ids", null), "db", null), "subnet_ids", null)
+  engine                  = each.value["engine"]
+  engine_version          = each.value["engine_version"]
+  replicas_per_node_group = each.value["replicas_per_node_group"]
+  num_node_groups         = each.value["num_node_groups"]
+  vpc_id                  = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  sg_subnet_cidr          = lookup(lookup(lookup(lookup(var.vpc, "main", null), "subnets", null), "app", null), "cidr_block", null)
+  node_type               = each.value["node_type"]
+  parameter_group_name    = each.value["parameter_group_name"]
+
+  tags        = var.tags
+  env         = var.env
+  kms_key_arn = var.kms_key_arn
+}
+
+
+
+
+# module "instances" {
+#   for_each = var.components
+#   source   = "git::https://github.com/Deepulucky/tf-module-app.git"
+#   component = each.key
+#   env       = var.env
+#   tags      = merge(each.value["tags"], var.tags)
+# }
